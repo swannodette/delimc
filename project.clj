1b144db79ff4591505583ea16e8584fcc971e558
David Nolen <david.nolen@gmail.com>
2011-04-28 20:27:32 -0400

    * project.clj: update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject delimc "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha5"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+(defproject delimc "0.1.0"
+  :description "Delimited Continuations for Clojure"
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]
+                     [lein-clojars "0.6.0"]])
