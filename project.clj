8c6daca28647c7eb3172072d73e3ab845239c60b
Brandon Bloom <brandon@brandonbloom.name>
2012-04-23 22:44:38 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject delimc "0.1.0"
   :description "Delimited Continuations for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]
-                     [lein-clojars "0.6.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
